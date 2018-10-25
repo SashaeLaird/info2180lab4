dd225c222b06a50adad77a029cf4d8206f5b199a
SashaeLaird <22225579+SashaeLaird@users.noreply.github.com>
2018-10-25 01:16:00 -0700

    Update maze.js

--- maze.js
+++ maze.js
@@ -1,50 +1,92 @@
-var loser = null;
- 
-window.onload = function() {
-    $("start").onclick = start;
-    $("start").onmouseover = startHover;
-    $("end").onmouseover = end;
-    var boundaries = $$("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].onmouseover = boundaryHandler;
-    }
-};
- 
-function startHover() {
-    if(loser === null) {
-        loser = false;
-        $("status").textContent = "Find the end!";
-    }
+//var tlboundary;
+var trackBoundary = false; //Variable used to track if the boundary has been hit
+window.onload = allBoundaries;
+//window.onload = topLeftBoundary;
+function topLeftBoundary() {
+   tlboundary = document.getElementById("boundary1");
+   tlboundary.onmouseover = redBoundary;
 }
- 
-function start() {
-    loser = false;
-    $("status").textContent = "Find the end!";
+
+function redBoundary() {
+    tlboundary.classList.add("youlose");
+}
+
+/*Exercise 2*/
+
+//Works but only one boundary get coloured at a time
+/*function allBoundaries() {
+    const boundaries = document.querySelectorAll(".boundary");
+    for (let i = 0; i < boundaries.length; i++) {
+        boundaries[i].addEventListener("mouseover", function() {
+            boundaries[i].classList.add("youlose");
+        });
+    }
+}*/
+
+function allBoundaries() {
+    const end = document.getElementById("end");
+    const start = document.getElementById("start");
+    start.onclick = restartMaze;
+    //end.onmouseover = messageAlert; /*Used for alert messages*/
+    const boundaries = document.querySelectorAll(".boundary");
+
+    for (let i = 0; i < boundaries.length; i++) {
+        boundaries[i].addEventListener("mouseover", allRedBoundary);
+    }
+    preventCheating(); //Call Function
+    end.onmouseover = statusUpdates;
+}
+
+function allRedBoundary() {
+    trackBoundary = true;
+    const boundaries = document.querySelectorAll(".boundary");
+
+    for (let i = 0; i < boundaries.length; i++) {
+        boundaries[i].classList.add("youlose");
+    }
     
-    var boundaries = $$("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].removeClassName("youlose");
+}
+
+/*Exercise 3*/
+
+function messageAlert() { //For alert messages
+    if (trackBoundary)
+    {
+        alert("You lose, mate!");
+    }
+    else{
+        alert("You win!");
+    } 
+}
+
+/*Exercise 4*/
+
+function restartMaze() {
+    trackBoundary = false;
+    document.getElementById("status").innerHTML = 'Move your mouse over the "S" to begin.';
+    const boundaries = document.querySelectorAll(".boundary");
+
+    for (let i = 0; i < boundaries.length; i++) {
+        boundaries[i].classList.remove("youlose");
     }
 }
 
+/*Exercise 5*/
 
-function end() {
-    if(loser === false) {
-        $("status").textContent = "Congratulation you won";
-        loser = null;
+function statusUpdates() {
+    if (trackBoundary) 
+    {
+        document.getElementById("status").innerHTML = "You lose, mate!";
     }
-    alert("Congratulation you won");
+    else 
+    {
+        document.getElementById("status").innerHTML = "You win!";
+    }
 }
- 
-function boundaryHandler() {
-    if (loser === false) {
-        loser = true;
-        $("status").textContent = "You lose!";
 
-        var boundaries = $$("div#maze div.boundary");
-        for (var i = 0; i < boundaries.length; i++) {
-            boundaries[i].addClassName("youlose");
-        }
+/*Exercise 6*/
 
-    }
+function preventCheating() {
+    var maze = document.getElementById("maze");
+    maze.onmouseleave = allRedBoundary;
 }
